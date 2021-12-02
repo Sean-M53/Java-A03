606e375fe5a9a63a2219a735aed35073a961d41f
Sean Marley <68084845+Sean-M53@users.noreply.github.com>
2021-12-02 17:44:06 +0000

    Add files via upload

---
+++ Assignment-3/src/main/java/com/example/assignment3/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.assignment3;
+
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+public class HelloController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Label welcomeText;
+
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
